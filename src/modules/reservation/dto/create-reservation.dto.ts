cbd51e170c02946359c6f63a789f7c8fc5ece6fb
SebasRojasM1 <sebasrojasm1@gmail.com>
2024-06-21 07:23:13 -0500

    Created the DTOs of the entities, with class-validator

--- src/modules/reservation/dto/create-reservation.dto.ts
+++ src/modules/reservation/dto/create-reservation.dto.ts
@@ -1,1 +1,24 @@
-export class CreateReservationDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+
+export class CreateReservationDto {
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    status: string;
+    
+    @ApiProperty()
+    @IsNumber()
+    @IsNotEmpty()
+    user_id: number; 
+  
+    @ApiProperty()
+    @IsNumber()
+    @IsNotEmpty()
+    workspace_id: number;
+  
+    @ApiProperty()
+    @IsNumber()
+    @IsNotEmpty()
+    session_id: number;
+}
--- src/modules/workspace/dto/create-workspace.dto.ts
+++ src/modules/workspace/dto/create-workspace.dto.ts
@@ -1,1 +1,19 @@
-export class CreateWorkspaceDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsNumber } from "class-validator";
+
+export class CreateWorkspaceDto {
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsNumber()
+    n_row: number;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsNumber()
+    n_column: number;
+
+    @ApiProperty()
+    @IsNumber()
+    @IsNotEmpty()
+    room_id: number;
+}
